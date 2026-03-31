25d815a6461c575c3740a619e544fcb6cfc8f084
yasmina baraza albertos <dj_yasminafter@hotmail.com>
2026-03-31 09:48:19 +0200

    feat: conexión prisma y endpoints de prueba

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,31 +1,52 @@
-/*import express from "express";
-import cors from "cors";
+import express from "express";
 import dotenv from "dotenv";
+import { prisma } from "./config/prisma";
 
 dotenv.config();
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Backend funcionando 🚀");
 });
 
-const PORT = process.env.PORT || 3001;
-
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+app.get("/test-db", async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json(users);
 });
-*/
 
-import express from "express";
+app.get("/create-user", async (req, res) => {
+  const user = await prisma.user.create({
+    data: {
+      email: "test@test.com",
+      username: "yasmin",
+      password: "123456",
+    },
+  });
 
-const app = express();
+  res.json(user);
+});
 
-app.get("/", (req, res) => {
-  res.send("Backend funcionando 🚀");
+app.post("/start-game", async (req, res) => {
+  try {
+    const game = await prisma.game.create({
+      data: {
+        userId: 1,
+        status: "active",
+        currentRoom: "room1",
+        state: {
+          firstPuzzleSolved: false,
+        },
+      },
+    });
+
+    res.json(game);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error al crear la partida" });
+  }
 });
 
 app.listen(3001, () => {
